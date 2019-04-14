705c3e6aa3b515595b88688c3664e372fa4c4f03
CL <cl3518@columbia.edu>
2019-04-14 18:18:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,18 @@
 #turn_count
-def turn_count()
+def turn_count
   count = 0 
   index.each do |move|
     if value == "X" || value == "O"
       count += 1
-    else put "Error"
+    end
     return count
+    
+def current_player(count)
+  if count % 2 == 0 
+    puts "It is the first player"
+  else 
+    puts "It is the second player"
+  end
+end
+    
     
